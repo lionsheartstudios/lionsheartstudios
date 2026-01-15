1734530328a296904eaa6d964ff46cdc14c606f5
guybriggs <guybriggs8278@gmail.com>
2026-01-15 14:44:31 +1100

    Hard-code prefixes

--- site-1/js/utils/addDirectoryPrefix.js
+++ site-1/js/utils/addDirectoryPrefix.js
@@ -1,20 +1,9 @@
 // addDirectoryPrefix.js
 
-const clean = (s = "") => String(s).replace(/^\/+|\/+$/g, "");
-const join = (...parts) => parts.map(clean).filter(Boolean).join("/");
-
-const PATH_PREFIX = env.VITE_PATH_PREFIX || env.PATH_PREFIX || "";
-const SITE_VERSION = env.VITE_SITE_VERSION || env.SITE_VERSION || "";
-
-const withLeadingSlash = (s = "") => {
-  const c = clean(s);
-  return c ? `/${c}` : "";
-};
-
 export const addPrefixAndSiteVersion = (path = "") => {
-  return withLeadingSlash(join(PATH_PREFIX, SITE_VERSION, path));
+  return "/lionsheartstudios/site-1/" + path;
 };
 
 export const addPrefixOnly = (path = "") => {
-  return withLeadingSlash(join(PATH_PREFIX, path));
+  return "/lionsheartstudios/" + path;
 };
