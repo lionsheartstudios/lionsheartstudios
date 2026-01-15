9b0729af2ca291aa0726e6bcc5dcc09bb6c16f3c
guybriggs <guybriggs8278@gmail.com>
2026-01-15 14:57:15 +1100

    Fixes for directories

--- site-1/js/utils/addDirectoryPrefix.js
+++ site-1/js/utils/addDirectoryPrefix.js
@@ -1,9 +1,11 @@
 // addDirectoryPrefix.js
 
 export const addPrefixAndSiteVersion = (path = "") => {
+  //return path;
   return "/lionsheartstudios/site-1/" + path;
 };
 
 export const addPrefixOnly = (path = "") => {
+  //return path;
   return "/lionsheartstudios/" + path;
 };
