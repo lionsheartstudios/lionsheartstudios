8c961dd652e1de79d4a2f3138493df8b100670c7
guybriggs <guybriggs8278@gmail.com>
2026-01-21 17:48:07 +1100

    Added special intro section, news page, copywriting wip 1

---
+++ 21-01-2026/js/utils/addDirectoryPrefix.js
@@ -0,0 +1,11 @@
+// addDirectoryPrefix.js
+
+export const addPrefixAndSiteVersion = (path = "") => {
+  //return "/" + path;
+  return "/lionsheartstudios/21-01-2026/" + path; // with trailing slash
+};
+
+export const addPrefixOnly = (path = "") => {
+  //return "/" + path;
+  return "/lionsheartstudios/" + path; // with trailing slash
+};
