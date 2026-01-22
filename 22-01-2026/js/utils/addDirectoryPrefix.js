59a9de2d1752a8d0c1dbd4b50513303a09e6c6fa
guybriggs <guybriggs8278@gmail.com>
2026-01-22 17:03:00 +1100

    22-01-2026 2

--- 22-01-2026/js/utils/addDirectoryPrefix.js
+++ 22-01-2026/js/utils/addDirectoryPrefix.js
@@ -1,11 +1,11 @@
 // addDirectoryPrefix.js
 
 export const addPrefixAndSiteVersion = (path = "") => {
-  return "/" + path;
-  return "/lionsheartstudios/tournaments-1/" + path; // with trailing slash
+  //return "/" + path;
+  return "/lionsheartstudios/22-01-2026/" + path; // with trailing slash
 };
 
 export const addPrefixOnly = (path = "") => {
-  return "/" + path;
+  //return "/" + path;
   return "/lionsheartstudios/" + path; // with trailing slash
 };
